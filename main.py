d48c53c49495bc432ef76bd11367a24e3224c4bf
djjurney <>
2022-12-16 15:26:45 +0000

    did you see this?

--- main.py
+++ main.py
@@ -7,19 +7,3 @@ for a minimum of 10 min a day.I'll be using Replit, an amazing
 online IDE so I can do this from my phone or wherever I happen to be. 
 No excuses for not coding fron the middle of a field.""")
 
-
-𝐜𝐥𝐚𝐬𝐬 𝐀𝐩𝐩𝐥𝐞:
-  𝐩𝐚𝐬𝐬
-
-𝐜𝐥𝐚𝐬𝐬 𝐎𝐫𝐚𝐧𝐠𝐞:
-  𝐩𝐚𝐬𝐬
-
-𝐚 = 𝐀𝐩𝐩𝐥𝐞()
-𝐛 = 𝐎𝐫𝐚𝐧𝐠𝐞()
-𝐛 = 𝐚
-
-𝐀 = 𝐭𝐲𝐩𝐞(𝐚)
-𝐁 = 𝐭𝐲𝐩𝐞(𝐀𝐩𝐩𝐥𝐞)
-𝐂 = 𝐭𝐲𝐩𝐞(𝐛)
-𝐃 = 𝐭𝐲𝐩𝐞(𝐎𝐫𝐚𝐧𝐠𝐞)
-print(type(B) == type(D))
